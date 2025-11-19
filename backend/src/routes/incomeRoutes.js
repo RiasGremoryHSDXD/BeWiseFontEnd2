2c084c9c60408071ec01764b78372bdc9d280dd4
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-11-19 22:59:28 +0800

    feat(income): added a update income endpoint

--- backend/src/routes/incomeRoutes.js
+++ backend/src/routes/incomeRoutes.js
@@ -3,11 +3,13 @@ import { protect } from "../middleware/authMiddleWare.js"
 import { addIncome } from "../controllers/incomeControllers/addIncomeControllers.js"
 import { readIncome } from "../controllers/incomeControllers/readIncomeControllers.js"
 import { deleteIncome } from "../controllers/incomeControllers/deleteIncomeControllers.js"
+import { updateIncome } from "../controllers/incomeControllers/updateIncomeControllers.js"
 
 const router = express.Router()
 
 router.post('/addIncome', protect, addIncome)
 router.get('/readIncome', protect, readIncome)
 router.delete('/deleteIncome/:id', protect, deleteIncome)
+router.put('/updateIncome/:id', protect, updateIncome)
 
 export default router;
